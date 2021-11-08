5aeb1c237ed21b2c409b4b932482530f55e3e598
rifkiystark <ananda.rifkiy32@gmail.com>
2021-11-09 04:01:44 +0700

    release v1

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Projects from './sections/projects/Projects';
 import Certificates from './sections/certificates/Certificates';
 
 function App() {
-  return (<>
-    <Cover />
-    <Projects />
-    <Certificates />
-  </>
+  return (
+    <>
+      <Cover />
+      <Projects />
+      <Certificates />
+      <div className="footer">Made with {'<3'} by <a rel="noreferrer" href="https://github.com/rifkiystark" target="_blank">rifkiystark</a></div>
+    </>
   );
 }
 
--- src/sections/cover/Cover.js
+++ src/sections/cover/Cover.js
@@ -5,10 +5,21 @@ function Cover() {
     return (
         <div className="section-cover">
             <div className="cover-header">
-                <img src={myProfile} alt="" />
+                <img src={myProfile} alt="" className="img-header"/>
                 <div className="title-header">ANANDA <span className="text-yellow">RIFKIY</span> HASAN</div>
                 <div className="divider-header"></div>
                 <code className="subtitle-header">Fullstack Developer</code>
+                <div className="wrapper-information-header">
+                    <a href="https://github.com/rifkiystark" className="information-header">Github</a>
+                    <div className="information-header">|</div> 
+                    <a href="mailto:ananda.rifkiy32@gmail.com" className="information-header">Email</a>
+                    <div className="information-header">|</div> 
+                    <a href="https://www.linkedin.com/in/anandarifkiy/" className="information-header">Linkedin</a>
+                    <div className="information-header">|</div> 
+                    <a href="https://wa.me/0895417459300" className="information-header">Phone</a>
+                    <div className="information-header">|</div> 
+                    <a href="https://www.dicoding.com/users/anandarifkiy/academies" className="information-header">Dicoding</a>
+                </div>
             </div>
         </div>
     )
