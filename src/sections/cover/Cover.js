a4da94d9ccd2e2d620abcb65079d7c2c63bd5c75
rifkiystark <ananda.rifkiy32@gmail.com>
2021-11-18 11:30:52 +0700

    update phon number

--- src/sections/cover/Cover.js
+++ src/sections/cover/Cover.js
@@ -10,17 +10,17 @@ function Cover() {
                 <div className="divider-header"></div>
                 <code className="subtitle-header">Fullstack Developer</code>
                 <div className="wrapper-information-header">
-                    <a href="https://github.com/rifkiystark" className="information-header">Github</a>
+                    <a href="https://github.com/rifkiystark" target="_blank" rel="noreferrer" className="information-header">Github</a>
                     <div className="information-header">|</div> 
-                    <a href="mailto:ananda.rifkiy32@gmail.com" className="information-header">Email</a>
+                    <a href="mailto:ananda.rifkiy32@gmail.com" target="_blank" rel="noreferrer" className="information-header">Email</a>
                     <div className="information-header">|</div> 
-                    <a href="https://www.linkedin.com/in/anandarifkiy/" className="information-header">Linkedin</a>
+                    <a href="https://www.linkedin.com/in/anandarifkiy/" target="_blank" rel="noreferrer" className="information-header">Linkedin</a>
                     <div className="information-header">|</div> 
-                    <a href="https://wa.me/0895417459300" className="information-header">Phone</a>
+                    <a href="https://wa.me/+62895417459300" target="_blank" rel="noreferrer" className="information-header">Phone</a>
                     <div className="information-header">|</div> 
-                    <a href="https://www.dicoding.com/users/anandarifkiy/academies" className="information-header">Dicoding</a>
+                    <a href="https://www.dicoding.com/users/anandarifkiy/academies" target="_blank" rel="noreferrer" className="information-header">Dicoding</a>
                     <div className="information-header">|</div> 
-                    <a href="https://www.instagram.com/anndrfky_/" className="information-header">Instagram</a>
+                    <a href="https://www.instagram.com/anndrfky_/" target="_blank" rel="noreferrer" className="information-header">Instagram</a>
                 </div>
             </div>
         </div>
